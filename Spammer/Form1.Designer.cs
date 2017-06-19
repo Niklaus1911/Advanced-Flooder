ca1d02cb432cbedf95a9aad0c193b150fd5d0589
Giuseppe <girailmondogira2@gmail.com>
2017-06-19 15:44:20 +0200

    Aggiungere i file di progetto.

---
+++ Spammer/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace Spammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Name:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(97, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(568, 210);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "erfwsrgv";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(245, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 21);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reload";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(150, 293);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 331);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "About";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(128, 252);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "10";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 18);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Sended Messages:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Max Messages:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(460, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Status:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(516, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Inactive";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(424, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Start Thread";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(505, 331);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "Stop Thread";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(297, 252);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 24);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.Text = "100";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(242, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 18);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Delay:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(326, 10);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 19;
+            this.checkBox1.Text = "Use by process";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 366);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Advanced Flooder - Inactive";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Spammer/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+
+namespace Spammer
+
+    
+{
+
+    public partial class Form1 : Form
+    {
+
+
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        int delay;
+        int var;
+        int valido = 0;
+        int max;
+        string menu;
+        int lbx;
+        string rc;
+        private delegate void del();
+        string lbl;
+
+        int check = 0;
+        public Form1()
+
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            label2.Text = "0";
+            timer1.Start();
+            this.Text = "Advanced Flooder - Active";
+            label6.Text = "Active";
+            label6.ForeColor = Color.Green;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var += 1;
+            label2.Text = var.ToString();
+            if (textBox2.Text == var.ToString())
+            {
+                timer1.Stop();
+                var = 0;
+
+                label6.Text = "Inactive";
+                label6.ForeColor = Color.Red;
+                this.Text = "Advanced Flooder - Inactive";
+
+            }
+
+            Process p = Process.GetProcessesByName("notepad").FirstOrDefault();
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle; //- sets the window 
+                SetForegroundWindow(h); //- sets the window or anything 
+                SendKeys.Send(richTextBox1.Text); //- this is message            
+                Thread.Sleep(300);  //- miliseconds before it hit ENTER
+                SendKeys.SendWait("{ENTER}"); //- hit enter to a new line
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            timer1.Stop();
+            var = 0;
+            this.Text = "Advanced Flooder - Inactive";
+            label6.Text = "Inactive";
+            label6.ForeColor = Color.Red;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+
+                ListViewItem item = new ListViewItem(theprocess.ProcessName);
+                comboBox1.Items.Add(item.Text);
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+
+                ListViewItem item = new ListViewItem(theprocess.ProcessName);
+                comboBox1.ResetText();
+                comboBox1.Items.Add(item.Text);
+
+            }
+        }
+
+
+
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Advanced Flooder\r\nAuthor: Niklaus1911\r\nAll rights reserved.\r\nWebsite: www.igdownloader.com", "Advanced Flooder", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                timer1.Start();
+            }
+
+            if (e.KeyCode == Keys.F6)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void fuori()
+        {
+            max = Int32.Parse(textBox2.Text) + 1;
+            label2.Text = lbl;
+            rc = richTextBox1.Text;
+
+            menu = comboBox1.Text;
+
+            lbx = Int32.Parse(textBox1.Text);
+        }
+
+        private void labelsix()
+        {
+            label6.Text = "Inactive";
+            label6.ForeColor = Color.Red;
+            this.Text = "Advanced Flooder - Inactive";
+        }
+
+        private void butto()
+        {
+            button4.Enabled = true;
+            button6.Enabled = false;
+        }
+
+       
+
+
+        private void Delegato()
+        {
+            del dl = fuori;
+            del lb6 = labelsix;
+            del but = butto;
+
+            while (1 == 1)
+            {
+                if (check==1)
+                {
+                    this.Invoke(dl);
+
+
+                    Process p = Process.GetProcessesByName(menu).FirstOrDefault();
+                    if (p != null)
+                    {
+                        IntPtr h = p.MainWindowHandle; //- sets the window 
+                        SetForegroundWindow(h); //- sets the window or anything 
+                        SendKeys.SendWait(rc); //- this is message            
+                        Thread.Sleep(lbx);//- miliseconds before it hit ENTER
+
+                        SendKeys.SendWait("{ENTER}"); //- hit enter to a new line
+
+
+                    }
+
+
+                    var += 1;
+                    lbl = var.ToString();
+                    if (max == var)
+                    {
+
+                        var = 0;
+
+
+                        this.Invoke(lb6);
+                        this.Invoke(but);
+
+                        break;
+                    }
+                    if (valido == 1)
+                    {
+                        valido = 0;
+                        break;
+
+                    }
+
+                }
+                else if (check==0)
+                {
+                    this.Invoke(dl);
+
+
+                    
+                    
+                       // IntPtr h = p.MainWindowHandle; //- sets the window 
+                      //  SetForegroundWindow(h); //- sets the window or anything 
+                        SendKeys.SendWait(rc); //- this is message            
+                        Thread.Sleep(lbx);//- miliseconds before it hit ENTER
+
+                        SendKeys.SendWait("{ENTER}"); //- hit enter to a new line
+
+
+                    
+
+
+                    var += 1;
+                    lbl = var.ToString();
+                    if (max == var)
+                    {
+
+                        var = 0;
+
+
+                        this.Invoke(lb6);
+                        this.Invoke(but);
+
+                        break;
+                    }
+                    if (valido == 1)
+                    {
+                        valido = 0;
+                        break;
+
+                    }
+                }
+                
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int delay;
+            delay= Int32.Parse(textBox1.Text);
+            if (delay<50)
+
+            {
+                MessageBox.Show("Minimum delay is 50 ms", "Advanced Flooder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+                
+            }
+            
+            
+
+            button4.Enabled = false;
+            button6.Enabled = true;
+
+            label2.Text = "0";
+            var = 0;
+            this.Text = "Advanced Flooder - Active";
+            label6.Text = "Active";
+            label6.ForeColor = Color.Green;
+            Thread thread1 = new Thread(Delegato);
+            thread1.Start();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            button6.Enabled = false;
+            valido = 1;
+            var = 0;
+            this.Text = "Advanced Flooder - Inactive";
+            label6.Text = "Inactive";
+            label6.ForeColor = Color.Red;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            valido = 1;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked==true)
+            {
+                check = 1;
+                button3.Enabled = true;
+                comboBox1.Enabled = true;
+                label1.Enabled = true;
+            }
+            else if (checkBox1.Checked==false)
+            {
+                check = 0;
+                button3.Enabled = false;
+                comboBox1.Enabled = false;
+                label1.Enabled = false;
+            }
+        }
+    }
+
+    }
+
